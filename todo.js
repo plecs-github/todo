f1a70777134f52bf87953865709a4f558325cc12
Plecs <plecs@t-online.hu>
2020-12-11 12:18:46 +0100

    Checkbox added

--- todo.js
+++ todo.js
@@ -3,7 +3,6 @@
 const todoInput = document.querySelector('.todo-input');
 const addTodoButton = document.querySelector('.add-todo');
 const todoContainer = document.querySelector('.todo-container');
-
 let storageId = 1;
 
 const deleteStorage = (id) => {
@@ -14,12 +13,16 @@ const deleteStorage = (id) => {
 const addDeleteEventListener = (id) => document.querySelector(`[data-id="${id}"]`)
          .addEventListener('click', () => deleteStorage(id));
 
+const createTodo = (text, id) => {
+   const todoItem = document.createElement('div');
+   todoItem.classList.add('todo-item');
+   todoItem.innerHTML = `<input type="checkbox" class="set-completed" name="set-completed" data-setid="${id}">${text} <button class="delete-button" data-id="${id}"><i class="far fa-trash-alt"></i></button>`;
+   todoContainer.insertBefore(todoItem, todoContainer.firstChild);
+}
+
 const addTodo = () => {
    if (todoInput.value) {
-      const todoItem = document.createElement('div');
-      todoItem.classList.add('todo-item');
-      todoItem.innerHTML = `${todoInput.value} <button class="delete-button" data-id="${storageId}"><i class="far fa-trash-alt"></i></button>`;
-      todoContainer.insertBefore(todoItem, todoContainer.firstChild);
+      createTodo (todoInput.value, storageId);
       localStorage.setItem(storageId.toString(), todoInput.value);
       addDeleteEventListener (storageId);
       todoInput.value = '';
@@ -28,10 +31,7 @@ const addTodo = () => {
 }
 
 Object.keys(localStorage).forEach((key) => {
-   const todoItem = document.createElement('div');
-   todoItem.classList.add('todo-item');
-   todoItem.innerHTML = `${localStorage.getItem(key)} <button class="delete-button" data-id="${key}"><i class="far fa-trash-alt"></i></button>`;
-   todoContainer.insertBefore(todoItem, todoContainer.firstChild);
+   createTodo(localStorage.getItem(key), key)
    addDeleteEventListener(key);
    if(parseInt(key) >= storageId) storageId = parseInt(key) + 1;
 });
